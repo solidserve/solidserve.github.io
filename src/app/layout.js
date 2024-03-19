ebbcd9909019924788e3a6b3c8779c364ef3ebeb
naybyal <officiallynabiel@gmail.com>
2024-03-20 00:22:02 +0530

    SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "solidServe",
+  title: "solidServe | All-round management system for Akshaya e-Centers",
   description: "All-round management system for Akshaya e-Centers",
 };
 
